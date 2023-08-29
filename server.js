d4e9db36de1f220bcd6f8037e4e7e58b589fa09c
CarsonFord <crf73@drexel.edu>
2023-08-29 16:41:36 -0400

    merged with main and fixed conflicts

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const crypto = require("crypto");
 const argon2 = require("argon2");
+const axios = require("axios");
 
 // Create express app with static handler
 const app = express();
